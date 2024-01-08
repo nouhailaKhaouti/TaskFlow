25bd9e0006ec7c173380377b0b17d4cca9571378
khaouti nouhaila <khaoutinouhaila@gmail.com>
2024-01-08 14:33:52 +0100

    create the jwtService so that i can retrieve username

--- src/main/java/com/example/taskflow/domaine/User.java
+++ src/main/java/com/example/taskflow/domaine/User.java
@@ -36,8 +36,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        /*return role.getAuthorities();*/
-        return null;
+        return role.getAuthorities();
     }
 
     @Override
--- src/main/java/com/example/taskflow/services/userService/UserServiceImpl.java
+++ src/main/java/com/example/taskflow/services/userService/UserServiceImpl.java
@@ -6,31 +6,45 @@ import com.example.taskflow.services.Dto.CustomUserDetails;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.Optional;
 import org.slf4j.Logger;
 
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserDetailsService {
-    private final UserRepository userRepository;
+public class UserServiceImpl /*implements UserDetailsService*/ {
+  /*  private final UserRepository userRepository;
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository repository;
 
-        logger.debug("Entering in loadUserByUsername Method...");
-        Optional<User> user = userRepository.findByUsername(username);
-        if(user.isEmpty()){
-            logger.error("Username not found: " + username);
-            throw new UsernameNotFoundException("could not found user..!!");
+    @Override
+    public void changePassword(ChangePasswordRequest request, Principal connectedUser) {
+
+        var user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        // check if the current password is correct
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalStateException("Wrong password");
         }
-        logger.info("User Authenticated Successfully..!!!");
-        return new CustomUserDetails(user.get());
-    }
+        // check if the two new passwords are the same
+        if (!request.getNewPassword().equals(request.getConfirmationPassword())) {
+            throw new IllegalStateException("Password are not the same");
+        }
+
+        // update the password
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+
+        // save the new password
+        repository.save(user);
+    }*/
 }
