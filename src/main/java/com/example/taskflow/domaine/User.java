0f4622c3c4fbc9588b2f2ddbf1c3df8335fb4308
khaouti nouhaila <khaoutinouhaila@gmail.com>
2024-01-08 11:47:16 +0100

    configure the application yaml configuration and create the user and role entities

--- src/main/java/com/example/taskflow/domaine/User.java
+++ src/main/java/com/example/taskflow/domaine/User.java
@@ -3,28 +3,70 @@ package com.example.taskflow.domaine;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
-@Entity
 @Data
-@ToString
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "USERS")
-public class User {
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "ID")
-    private Long id;
-    private String username;
-    @JsonIgnore
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
-    private Set<UserRole> roles = new HashSet<>();
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
+/*    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;*/
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        /*return role.getAuthorities();*/
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
